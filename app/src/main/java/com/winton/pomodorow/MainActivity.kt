a01043dac585b105df4e6be6baf82495be6891fc
WintonOPF <winton.porto@ufms.br>
2023-03-11 19:31:27 -0400

    feat: Dialog to change timer countdown

--- app/src/main/java/com/winton/pomodorow/MainActivity.kt
+++ app/src/main/java/com/winton/pomodorow/MainActivity.kt
@@ -4,19 +4,19 @@ import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.winton.pomodorow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
     private lateinit var timerMain: CountDownTimer
     private lateinit var timerBreak: CountDownTimer
     private lateinit var timerTen: CountDownTimer
 
     private var contador:Int = 0
-    private var m1:Long = 1500000 //25 minutos
     private var m2:Long = 300000 //5 minutos
 
     private var player: MediaPlayer? = null
@@ -27,51 +27,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         setupUI()
 
-        timerMain = object : CountDownTimer(m1+500,1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                val minuto :Int = (millisUntilFinished/60000).toInt()
-                val segundo : Int = (millisUntilFinished%60000/1000).toInt()
+        setupTimerMain(1500000)
 
-                val minutoFormatado:String = if (minuto < 10){
-                    "0$minuto"
-                } else
-                    minuto.toString()
-
-                val segundoFormatado:String = if (segundo < 10){
-                    "0$segundo"
-
-                } else
-                    segundo.toString()
-                val timer = "$minutoFormatado:$segundoFormatado"
-                binding.txtTimer.text = timer
-
-            }
-
-            override fun onFinish() {
-                playNotificationSound()
-                contador+=1
-                binding.txtContador.text = contador.toString()
-                if(contador % 4 == 0){
-                    val builder = MaterialAlertDialogBuilder(this@MainActivity)
-                    builder.setMessage("É recomendado um descanso de 10 minutos. Aceita ?")
-                        .setPositiveButton("SIM"){dialog,_->
-                            /*setar o timer de 10min*/
-                            dialog.dismiss()
-                            timerTen.start()
-                        }
-                        .setNegativeButton("NÃO"){dialog,_->
-                            //continuar o timer comum de 5  min
-                            dialog.dismiss()
-                            timerBreak.start()
-                        }
-                        .create().show()
-                }
-                else
-                    timerBreak.start()
-
-            }
+        binding.txtTimer.setOnClickListener {
+            timerMain.cancel()
+            setupTimerDialog()
         }
         
         timerBreak = object : CountDownTimer(m2+500,1000) {
@@ -138,6 +101,71 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setupTimerDialog() {
+        val builder = MaterialAlertDialogBuilder(this@MainActivity)
+        val view = layoutInflater.inflate(R.layout.change_timer_layout, null)
+        builder.setView(view)
+            .setPositiveButton("OK") { dialog, _ ->
+                val editText = view.findViewById<EditText>(R.id.et_timer)
+                if (editText.text.toString() != "" && editText.text.toString() != "0") {
+                    setupTimerMain((editText.text.toString().toLong()) * 60 * 1000)
+                    dialog.dismiss()
+                } else {
+                    Toast.makeText(this, "Preencha o campo corretamente!", Toast.LENGTH_LONG).show()
+                }
+            }
+            .setNegativeButton("CANCELAR") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create().show()
+    }
+
+    private fun setupTimerMain(valor :Long) {
+        timerMain = object : CountDownTimer(valor + 500, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val minuto: Int = (millisUntilFinished / 60000).toInt()
+                val segundo: Int = (millisUntilFinished % 60000 / 1000).toInt()
+
+                val minutoFormatado: String = if (minuto < 10) {
+                    "0$minuto"
+                } else
+                    minuto.toString()
+
+                val segundoFormatado: String = if (segundo < 10) {
+                    "0$segundo"
+
+                } else
+                    segundo.toString()
+                val timer = "$minutoFormatado:$segundoFormatado"
+                binding.txtTimer.text = timer
+
+            }
+
+            override fun onFinish() {
+                playNotificationSound()
+                contador += 1
+                binding.txtContador.text = contador.toString()
+                if (contador % 4 == 0) {
+                    val builder = MaterialAlertDialogBuilder(this@MainActivity)
+                    builder.setMessage("É recomendado um descanso de 10 minutos. Aceita?")
+                        .setPositiveButton("SIM") { dialog, _ ->
+
+                            dialog.dismiss()
+                            timerTen.start()
+                        }
+                        .setNegativeButton("NÃO") { dialog, _ ->
+
+                            dialog.dismiss()
+                            timerBreak.start()
+                        }
+                        .create().show()
+                } else
+                    timerBreak.start()
+
+            }
+        }
+    }
+
     private fun playNotificationSound(){
         try{
             val sound = Uri.parse("android.resource://com.winton.pomodorow/"+R.raw.som_pomodoro)
@@ -151,7 +179,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupUI(){
         binding.txtContador.text = contador.toString()
-        val timer = (m1/60000).toString() + ":" + "00"
+        val timer = (1500000/60000).toString() + ":" + "00"
         binding.txtTimer.text = timer
     }
 }
